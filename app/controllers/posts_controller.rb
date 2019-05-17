d6b9b0ca2813484ccd147f0c8c67ffa3693425cf
Won Kim <wonsae.kim@gmail.com>
2019-05-16 20:16:15 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,10 +12,11 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new
-	  @post.title = params[:title]
-	  @post.description = params[:description]
-	  @post.save
+		@post = Post.new
+		@post.title = params[:title]
+		@post.description = params[:description]
+		@post.save
+
 	  redirect_to post_path(@post)
 	end
 
@@ -25,7 +26,12 @@ class PostsController < ApplicationController
 
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(title: params[:title], description: params[:description])
+	  @post.update(post_params(:title, :description))
 	  redirect_to post_path(@post)
+	end
+
+	private
+	def post_params(*args)
+		params.require(:post).permit(*args)
 	end
 end
